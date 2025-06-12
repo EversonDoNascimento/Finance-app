ed9d03bfbc43ecb8e19051c65899726f90fcbb01
EversonDoNascimento <everson25nascimento@gmail.com>
2025-06-12 01:33:55 -0300

    feat: Added content in index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
+import { globalStyles } from "@/styles/global";
 import { Image, Text, View } from "react-native";
-
 const transactions = [
   { id: "1", description: "Supermercado", amount: -50.75 },
   { id: "2", description: "Salário", amount: 2500.0 },
@@ -9,19 +9,39 @@ const transactions = [
 
 export default function Index() {
   return (
-    <View>
-      <Image source={require("@/assets/images/logo.png")} />
-      <Text>Saldo Atual</Text>
-      <Text>R$ 1.529,85</Text>
-      <View>
-        <Text>Adicionar Receita</Text>
-        <Text>Adicionar Despesa</Text>
+    <View style={globalStyles.container}>
+      <Image
+        style={globalStyles.logo}
+        source={require("@/assets/images/logo.png")}
+      />
+      <Text style={globalStyles.greeting}>Olá, usuário!</Text>
+
+      <Text style={globalStyles.balanceLabel}>Saldo Atual</Text>
+      <Text style={globalStyles.balance}>R$ 1.529,85</Text>
+      <View style={globalStyles.buttonsContainer}>
+        <View style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>Adicionar Receita</Text>
+        </View>
+        <View style={globalStyles.button}>
+          <Text style={globalStyles.buttonText}>Adicionar Despesa</Text>
+        </View>
       </View>
-      <Text>Transações Recentes</Text>
+      <Text style={globalStyles.sectionTitle}>Transações Recentes</Text>
       {transactions.map((transaction) => (
-        <View key={transaction.id}>
-          <Text>{transaction.description}</Text>
-          <Text>R$ {transaction.amount.toFixed(2)}</Text>
+        <View style={globalStyles.transactionItem} key={transaction.id}>
+          <Text style={globalStyles.transactionText}>
+            {transaction.description}
+          </Text>
+          <Text
+            style={[
+              globalStyles.transactionAmount,
+              transaction.amount < 0
+                ? globalStyles.expense
+                : globalStyles.income,
+            ]}
+          >
+            R$ {transaction.amount.toFixed(2)}
+          </Text>
         </View>
       ))}
     </View>
